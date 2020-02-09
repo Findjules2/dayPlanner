81a3be9f11ceb92ff3fdf4b8aa9e087cccbca607
Julie Berryhill <juliesellsnc@gmail.com>
2020-02-09 18:20:22 -0500

    Working day planner

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # dayPlanner
+
+## Create a day planner that shows current hour highlighted and grays out past events. This is just for the work day of 9 a.m. to 5 p.m.. You must be able to add and save events for each hour of the work day.
+
+* The URL of the GitHub repository
+
+https://github.com/Findjules2/dayPlanner
+
+* The URL of the deployed application
+
+https://findjules2.github.io/dayPlanner/
+
+![Dayplanner Screenshot](./assets/images/dayplanner.png)
---
+++ assets/script.js
@@ -0,0 +1,43 @@
+//click function to save text
+$(".saveBtn").on("click", function() {
+    // get ElementByID
+    //save text to blank variable
+    console.log($(this.dataset.time))
+    var inputText = $(this.dataset.time).val()
+    console.log(inputText)
+    //save blank variable to local storage
+    var inputTextArray = JSON.parse(localStorage.getItem("inputTextArray") || "[]")
+    var newInput = {
+        text: inputText,
+        id: this.dataset.time
+    }
+    inputTextArray.push(newInput)
+    localStorage.setItem("inputTextArray", JSON.stringify(inputTextArray))
+})
+
+//get from local storage and display in timeBlock
+var inputTextArray = JSON.parse(localStorage.getItem("inputTextArray") || "[]")
+inputTextArray.forEach(function(obj) {
+    $(obj.id).val(obj.text)
+})
+
+$("#currentDay").text(Date(Date.now()))  
+//var currentDateAndTime = Date(Date.now());// exp: Fri Oct 25 2019 17:40:56 GMT-0400 (Eastern Daylight Time)
+var timeChange = setInterval(
+    function () {
+    $("#currentDay").text(Date(Date.now()))   
+}, 1000)
+
+var currentHour = new Date().getHours();//Current hour in military time`
+
+console.log(currentHour)
+
+$('#timeBlock'+currentHour).removeClass('future')
+$('#timeBlock'+currentHour).addClass('present')
+
+$(document).ready(function(){
+    for (var t =9; t < currentHour; t++) {
+        $('#timeBlock'+t).removeClass('future')
+    $('#timeBlock'+t).addClass('past') 
+    }
+    });
